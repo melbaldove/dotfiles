8c05a529bf8db1882879174495b71d02d6d153a4
Melbourne Baldove <melbournebaldove@gmail.com>
2025-06-30 22:15:07 +0800

    fix: comment out tex definition to prevent builds

--- users/melbournebaldove/dev.nix
+++ users/melbournebaldove/dev.nix
@@ -3,16 +3,16 @@
   config, pkgs, self, ...
 }:
 let
-  tex = (pkgs.texlive.combine {
-    inherit (pkgs.texlive) scheme-basic
-      luatex
-      fontspec
-      xcolor
-      mylatexformat
-      preview
-      dvisvgm dvipng # for preview and export as html
-      wrapfig amsmath ulem hyperref capt-of;
-  });
+  # tex = (pkgs.texlive.combine {
+  #   inherit (pkgs.texlive) scheme-basic
+  #     luatex
+  #     fontspec
+  #     xcolor
+  #     mylatexformat
+  #     preview
+  #     dvisvgm dvipng # for preview and export as html
+  #     wrapfig amsmath ulem hyperref capt-of;
+  # });
   
 in
 {
