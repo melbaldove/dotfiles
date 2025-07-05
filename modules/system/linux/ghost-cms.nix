eec4b297ce3167139bb417cf1c2c204c1c9eee6d
Melbourne Baldove <melbournebaldove@gmail.com>
2025-07-05 20:22:00 +0800

    fix: revert to correct secret handling approach
    - Revert twenty-crm.nix and ghost-cms.nix to read secrets at build time
    - Revert arion-compose files to use direct password values
    - NixOS modules handle secret reading with builtins.readFile
    - Containers receive resolved password values, not file paths
    
    This fixes the pure evaluation mode error by ensuring secrets are
    read at the correct time during the build process.

--- modules/system/linux/ghost-cms.nix
+++ modules/system/linux/ghost-cms.nix
@@ -89,6 +89,18 @@ with lib;
           description = "Use secure SMTP connection";
         };
 
+        user = mkOption {
+          type = types.nullOr types.str;
+          default = null;
+          description = "SMTP username";
+        };
+
+        password = mkOption {
+          type = types.nullOr types.str;
+          default = null;
+          description = "SMTP password";
+        };
+
         userFile = mkOption {
           type = types.nullOr types.path;
           default = null;
@@ -152,7 +164,24 @@ with lib;
         imports = [ 
           (import "${inputs.ghost}/ghost-arion-compose.nix" {
             inherit pkgs lib;
-            config = config.services.ghost-cms;
+            config = config.services.ghost-cms // {
+              # Read secrets at build time and pass actual values
+              database = config.services.ghost-cms.database // {
+                password = if config.services.ghost-cms.database.passwordFile != null
+                  then builtins.readFile config.services.ghost-cms.database.passwordFile
+                  else "ghostpass";
+              };
+              mail = config.services.ghost-cms.mail // {
+                smtp = config.services.ghost-cms.mail.smtp // {
+                  user = if config.services.ghost-cms.mail.smtp.userFile != null
+                    then builtins.readFile config.services.ghost-cms.mail.smtp.userFile
+                    else config.services.ghost-cms.mail.smtp.user;
+                  password = if config.services.ghost-cms.mail.smtp.passwordFile != null
+                    then builtins.readFile config.services.ghost-cms.mail.smtp.passwordFile
+                    else config.services.ghost-cms.mail.smtp.password;
+                };
+              };
+            };
           })
         ];
       };
