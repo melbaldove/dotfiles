151473f96a312815595723d7e1a2499b5bc20ac4
Melbourne Baldove <melbournebaldove@gmail.com>
2025-07-17 17:08:13 +0800

    fix: remove existing pulse symlink before creating new one

--- hosts/newton/default.nix
+++ hosts/newton/default.nix
@@ -337,6 +337,7 @@
       echo "DB_POSTGRESDB_PASSWORD=$(cat $CREDENTIALS_DIRECTORY/n8n-db-password)" > /tmp/n8n-env
       
       # Create symlink to pulse in working directory
+      rm -f pulse
       ln -sf ${inputs.pulse} pulse
     '';
     serviceConfig = {
