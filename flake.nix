f8042ce0a3a81debb12b916fac5c1567dbb4d9cf
Melbourne Baldove <melbournebaldove@gmail.com>
2025-09-21 11:02:26 +0800

    refactor: remove infrastructure and services from dotfiles

--- flake.nix
+++ flake.nix
@@ -9,14 +9,8 @@
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     nix-homebrew.url = "github:zhaofengli/nix-homebrew";
-    deploy-rs.url = "github:serokell/deploy-rs";
     agenix.url = "github:ryantm/agenix";
     agenix.inputs.nixpkgs.follows = "nixpkgs";
-
-    twenty.url = "github:next-desk-business-solutions/twenty";
-    ghost.url = "github:next-desk-business-solutions/Ghost";
-    outline.url = "github:next-desk-business-solutions/outline";
-    pulse.url = "github:next-desk-business-solutions/pulse";
 
     emacs-overlay.url = "github:nix-community/emacs-overlay";
     emacs-overlay.inputs.nixpkgs.follows = "nixpkgs";
@@ -38,7 +32,7 @@
 
   };
 
-  outputs = inputs@{ self, nix-darwin, home-manager, nix-homebrew, nixpkgs, deploy-rs, ... }:
+  outputs = inputs@{ self, nix-darwin, home-manager, nix-homebrew, nixpkgs, ... }:
   {
     darwinConfigurations."turing" = nix-darwin.lib.darwinSystem {
       specialArgs = { inherit inputs self; };
@@ -46,63 +40,5 @@
         ./hosts/turing/default.nix
       ];
     };
-
-    nixosConfigurations."einstein" = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      specialArgs = { inherit inputs self; };
-      modules = [ 
-        ./hosts/einstein/default.nix
-      ];
-    };
-
-    nixosConfigurations."shannon" = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      specialArgs = { inherit inputs self; };
-      modules = [ 
-        ./hosts/shannon/default.nix
-      ];
-    };
-
-    nixosConfigurations."newton" = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      specialArgs = { inherit inputs self; };
-      modules = [ 
-        ./hosts/newton/default.nix
-      ];
-    };
-
-    deploy.nodes.einstein = {
-      hostname = "einstein";
-      remoteBuild = true;
-      profiles.system = {
-        user = "root";
-        path = deploy-rs.lib.x86_64-linux.activate.nixos 
-          self.nixosConfigurations.einstein;
-      };
-    };
-
-    deploy.nodes.shannon = {
-      hostname = "shannon";
-      remoteBuild = true;
-      magicRollback = false;
-      profiles.system = {
-        user = "root";
-        path = deploy-rs.lib.x86_64-linux.activate.nixos 
-          self.nixosConfigurations.shannon;
-      };
-    };
-
-    deploy.nodes.newton = {
-      hostname = "newton";
-      profiles.system = {
-        user = "root";
-        path = deploy-rs.lib.x86_64-linux.activate.nixos 
-          self.nixosConfigurations.newton;
-      };
-    };
-
-    checks = builtins.mapAttrs 
-      (system: deployLib: deployLib.deployChecks self.deploy) 
-      deploy-rs.lib;
   };
 }
